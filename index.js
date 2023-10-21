e1156f2c3f541e9fbd6d6c84f3e0f44933d15ecc
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-10-21 11:13:03 -0300

    (Feature): Deleted all db data

--- index.js
+++ index.js
@@ -18,14 +18,10 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const getTypes = require('./src/controllers/getTypes.js');
-const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
-    console.log('%s listening at port ' + port); // eslint-disable-line no-console
-  });
+server.listen(port, () => {
+  console.log('Server listening at port ' + port); // eslint-disable-line no-console
 });
--- src/controllers/getTypes.js
+++ src/controllers/getTypes.js
@@ -4,26 +4,10 @@ const axios = require("axios");
 const getTypes = async (req, res) => {
   try {
 
-    let types = await Type.findAll();
+    const response = await axios.get('https://pokeapi.co/api/v2/type');
+    const apiTypes = response.data.results;
 
-    // Si la base de datos está vacía, obtenemos los tipos desde la API
-    if (types.length === 0) {
-      const response = await axios.get('https://pokeapi.co/api/v2/type');
-      const apiTypes = response.data.results;
-
-      // Mapeamos los datos de los tipos para guardarlos en la base de datos
-      types = await Promise.all(
-        apiTypes.map(async (type, index) => {
-          const typeData = {
-            id: index + 1, // Usamos el índice + 1 como id
-            name: type.name,
-          };
-          return await Type.create(typeData);
-        })
-      );
-    }
-
-    res.status(200).json(types);
+    res.status(200).json(apiTypes);
   } catch (error) {
     res.status(500).json(error.message);
   }
